a07296625b034e1f717d51b0d1a9ef8fffa966c6
Domange <backinsmail@gmail.com>
2018-11-06 16:27:56 +0100

    [PROGRESS] exercice 1 string

---
+++ exercice-1/string.js
@@ -0,0 +1,102 @@
+function testifyStr(str)
+{
+	if(typeof str == "string" && str.length > 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function ucfirst(str)
+{
+	if(testifyStr(str)){
+		return str[0].toUpperCase() + str.slice(1);
+	} else {
+		return false;
+	}
+}
+
+function lcfirst(str)
+{
+	if(testifyStr(str)){
+		return str[0].toLowerCase() + str.slice(1);
+	} else {
+		return false;
+	}
+}
+
+function capitalize(str)
+{
+
+	if(testifyStr(str)){
+		var tmp = str.split(" "), streturn = "";
+		for (i in tmp){
+			streturn += ucfirst(tmp[i]) + " ";
+		}
+		return streturn;
+	} else {
+		return false;
+	}
+}
+
+function camelCase(str)
+{
+	if(testifyStr(str)){
+		str = str.toLowerCase();
+		var tmp = capitalize(str).split(" ").join("");
+		return lcfirst(tmp);
+	} else {
+		return false;
+	}
+}
+
+function snake_case(str)
+{
+	if(testifyStr(str)){
+		return str.split(" ").join("_").toLowerCase();
+	} else {
+		return false;
+	}
+}
+
+function leet(str)
+{
+	if(testifyStr(str)){
+		var tmp = "";
+		for(l in str){
+			switch(str[l].toUpperCase()){
+				case "A":
+					tmp += "4";
+					break;
+				case "E":
+					tmp += "3";
+					break;
+				case "I":
+					tmp += "1";
+					break;
+				case "O":
+					tmp += "o";
+					break;
+				case "U":
+					tmp += "(_)";
+					break;
+				case "Y":
+					tmp += "7";
+					break;
+				default:
+					tmp += str[l];
+					break;
+			}
+		}
+		return tmp;
+	} else {
+		return false;
+	}
+}
+
+
+console.log(ucfirst(""));
+console.log(capitalize("hello world !"));
+console.log(camelCase("Hello world WAHOU!"));
+console.log(snake_case("hello world !"));
+console.log(leet("a b c d e f g h i j k l m n o p q r s t u v w x y z"));
