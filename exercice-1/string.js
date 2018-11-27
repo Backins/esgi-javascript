322e8109eb7a64be4f2d1714a22a2b9c4bed81e6
Domange <backinsmail@gmail.com>
2018-11-27 16:51:18 +0100

    [FIX] Bug fix after checker report

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -1,133 +1,140 @@
-function testifyStr(str)
-{
-	if(typeof str === "string" && str.length > 0) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function ucfirst(str)
-{
-	if(testifyStr(str)){
-		return str[0].toUpperCase() + str.slice(1);
-	} else {
-		return "";
-	}
-}
-
-function lcfirst(str)
-{
-	if(testifyStr(str)){
-		return str[0].toLowerCase() + str.slice(1);
-	} else {
-		return "";
-	}
-}
-
-function capitalize(str)
-{
-	if(testifyStr(str)){
-		str = str.toLowerCase();
-		var tmp = str.split(" "), streturn = "";
-		for (i in tmp){
-			streturn += ucfirst(tmp[i]);
-			if(i < tmp.length - 1) {
-				streturn += " ";
-			}
-		}
-		return streturn;
-	} else {
-		return "";
-	}
-}
-
-function camelCase(str)
-{
-	if(testifyStr(str)){
-		str = str.replace(/[\W_]+/g," ");
-		str = capitalize(str).split(" ").join("");
-		str = str.replace("_",' ');
-		return str;
-	} else {
-		return "";
-	}
-}
-
-function snake_case(str)
-{
-	if(testifyStr(str)){
-		return str.split(" ").join("_").toLowerCase();
-	} else {
-		return "";
-	}
-}
-
-function leet(str)
-{
-	if(testifyStr(str)){
-		let tmp = "";
-		for(var l in str){
-			switch(str[l].toUpperCase()){
-				case "A":
-					tmp += "4";
-					break;
-				case "E":
-					tmp += "3";
-					break;
-				case "I":
-					tmp += "1";
-					break;
-				case "O":
-					tmp += "0";
-					break;
-				case "U":
-					tmp += "(_)";
-					break;
-				case "Y":
-					tmp += "7";
-					break;
-				default:
-					tmp += str[l];
-					break;
-			}
-		}
-		return tmp;
-	} else {
-		return "";
-	}
-}
-
-function verlan(str)
-{
-	if(testifyStr(str)){
-        return str.split("").reverse().join("");
-	} else {
-		return "";
-	}
-}
-
-function yoda(str)
-{
-	if(testifyStr(str)){
-        return str.split(" ").reverse().join(" ");
-	} else {
-		return "";
-	}
-}
-
-function prop_access(input, path){
-    let pathArray = path.split(".");
-    for(let i = 0; i < pathArray.length; i++){
-        if(input[pathArray[i]] === undefined){
-            console.error('path exist');
-            return null;
-        }
-        input = input[pathArray[i]];
+function ucfirst(str) {
+    if (typeof str !== 'string') {
+        return '';
     }
-    return input;
+
+    return str.charAt(0).toUpperCase() + str.substring(1);
 }
 
-// For best result visibility
-// ->Result<-
-console.log("->"+yoda(null)+"<-");
+function capitalize(str) {
+    if (typeof str !== 'string') {
+        return '';
+    }
+
+    let temp = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (!str[i - 1] || str[i - 1] === ' ') {
+            temp += str[i].toUpperCase();
+        } else {
+            temp += str[i].toLowerCase();
+        }
+    }
+
+    return temp;
+}
+
+function camelCase(str) {
+    if (typeof str !== 'string') {
+        return '';
+    }
+
+    let temp = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (!str[i - 1] || str[i - 1] === ' ' || str[i - 1] === '_') {
+            temp += str[i].toUpperCase();
+        } else {
+            temp += str[i].toLowerCase();
+        }
+    }
+
+    return temp.replace(/[ _]/g, '');
+}
+
+function snake_case(str) {
+    if (typeof str !== 'string') {
+        return '';
+    }
+
+    return str.replace(/ /g, '_').toLowerCase();
+}
+
+function leet(str) {
+    if (typeof str !== 'string') {
+        return '';
+    }
+
+    const voyelles = { 'A': '4', 'E': '3', 'I': '1', 'O': '0', 'U': '(_)', 'Y': '7' };
+    let temp = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (voyelles[str[i].toUpperCase()]) {
+            temp += voyelles[str[i].toUpperCase()];
+        } else {
+            temp += str[i];
+        }
+    }
+
+    return temp;
+}
+
+function verlan(str) {
+    if (typeof str !== 'string') {
+        return '';
+    }
+
+    let temp = str.split(' ');
+    var verlan = [];
+
+    for (let i = 0; i < temp.length; i++) {
+        let toBeReversed = temp[i].split('');
+        let reversedArray = toBeReversed.reverse();
+        verlan.push(reversedArray.join(''));
+    }
+
+    return verlan.join(' ');
+}
+
+function yoda(str) {
+    if (typeof str !== 'string') {
+        return '';
+    }
+
+    return str.split(' ').reverse().join(' ');
+}
+
+function vig(mot, clef) {
+    if (typeof mot != "string" || typeof clef != "string"){
+        return "";
+    }
+    motCrypte = "";
+    cptSpace = 0;
+    for(var i = 1;i <= mot.length; i++) {
+        if(mot.charAt(i-1) != " ") {
+            codeLettre = mot.toUpperCase().charCodeAt(i-1)-65;
+            decalage = clef.toUpperCase().charCodeAt((i-1-cptSpace)%clef.length)-65;
+            lettre = String.fromCharCode(65+(codeLettre+decalage)%26);
+            motCrypte = motCrypte.concat('', lettre);
+        } else {
+            cptSpace++
+            motCrypte = motCrypte.concat('', ' ');
+        }
+    }
+    return motCrypte.toLowerCase();
+}
+
+function prop_access(object, path) {
+    if (typeof path != "string"){
+        return object;
+    }
+
+    if(typeof object != 'object' || object == null) {
+        console.log(path + ' not exist');
+        return;
+    }
+    if (path === null || path === '') {
+        return object;
+    }
+
+    const props = path.split('.');
+    let property = object;
+    props.forEach(function (prop) {
+        if(!property.hasOwnProperty(prop)) {
+            console.log(path + ' not exist');
+            return;
+        }
+        property = property[prop];
+    });
+    return property;
+}
